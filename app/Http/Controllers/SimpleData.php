9a4e79e92888b1e7c2562157198d1808dfd960b5
milorad996 <mips96.1@gmail.com>
2022-09-21 16:04:49 +0200

    #CSDT-19

---
+++ app/Http/Controllers/SimpleData.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Requests\DataRequest;
+class SimpleData extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(
+            [
+            
+                    "name" => "Route that returns some data",
+                    "type"=> "GET",
+                    "description"=> "This route returns some data"
+               
+            ]
+        )
+            
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DataRequest $request)
+    {
+        $request->validated();
+
+        $value = $request->body('value')
+
+        if($value == "some value"){
+            return response()->json([
+                "value" => "some value - modified"
+            ])
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
