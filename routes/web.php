2748b3c399f7015bcf49c9da583435b5b7273009
milorad996 <mips96.1@gmail.com>
2022-09-21 16:45:21 +0200

    #CSDT-21

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/simple',function() {
+    return view('simple',['first_name' => "Milorad"]);
+});
